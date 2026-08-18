52ea887a38fe6d4566d2a910346523b65ad8c81a
Rohan jangir 13 <jangirrohan31@gmail.com>
2026-08-18 15:09:21 +0530

    Restore CTA in Footer and remove Statistics ribbon from About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,12 +1,10 @@
 import PageTransition from '../components/PageTransition';
 import AboutComponent from '../components/About';
-import CredibilityBand from '../components/Statistics';
 
 export default function About() {
   return (
     <PageTransition>
       <AboutComponent />
-      <CredibilityBand />
     </PageTransition>
   );
 }
