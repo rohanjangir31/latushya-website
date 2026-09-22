c6bcac1eae879310e5b45921b1c720b476b43d87
Rohan jangir 13 <jangirrohan31@gmail.com>
2026-09-22 15:51:53 +0530

    cleanup: remove scratch scripts from repo tracking

--- update_captions.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let code = fs.readFileSync('src/data/projects.js', 'utf8');
-
-code = code.replace(/title: 'Cosmopolitan Living Space',/, "title: 'Assetz Mark',");
-code = code.replace(/caption: 'Real site living space view \d+\.'/g, "caption: ''");
-
-fs.writeFileSync('src/data/projects.js', code);
-console.log('Project updated successfully.');
